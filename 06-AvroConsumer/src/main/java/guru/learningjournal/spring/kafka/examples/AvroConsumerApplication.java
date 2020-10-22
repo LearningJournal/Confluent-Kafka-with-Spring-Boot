313e8ff9cd622ade7fd3a1197c0754a1bd7869c4
Prashant Kumar Pandey <LearningJournal@users.noreply.github.com>
2020-10-22 15:44:53 +0530

    Initial Commit

---
+++ 06-AvroConsumer/src/main/java/guru/learningjournal/spring/kafka/examples/AvroConsumerApplication.java
@@ -0,0 +1,13 @@
+package guru.learningjournal.spring.kafka.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroConsumerApplication.class, args);
+	}
+
+}
